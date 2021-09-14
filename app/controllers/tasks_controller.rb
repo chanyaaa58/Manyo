e21a301e558187b44fd95bbc08b9a69e4ddad34f
chanyaaa58 <kanegonaja@gmail.com>
2021-09-14 16:46:06 +0900

    タスクの一覧、新規作成、詳細、編集画面を作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update]
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを登録しました！"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:list, :detail)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
