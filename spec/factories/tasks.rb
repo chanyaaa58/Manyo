b3956d35b5a0223a73e8d5a89e2017a5a5f8c8b1
chanyaaa58 <kanegonaja@gmail.com>
2021-09-16 17:42:07 +0900

    一覧画面で作成日順に表示されるよう実装、System Specで作成時の降順での並び替えテストを実行

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,10 +1,14 @@
 FactoryBot.define do
-    factory :task do
-      list { 'test_list' }
-      detail { 'test_detail' }
-    end
-    factory :second_task, class: Task do
-        list { 'test_list2' }
-        detail { 'test_detail2' }
-    end
+  # 作成するテストデータの名前を「task」とします
+  # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
+  factory :task do
+    list { 'test_list' }
+    detail { 'test_detail' }
   end
+  # 作成するテストデータの名前を「second_task」とします
+  # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
+  factory :second_task, class: Task do
+    list { 'test_list2' }
+    detail { 'test_detail2' }
+  end
+end
