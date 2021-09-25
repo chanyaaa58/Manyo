0a08b7f6f78891c13528fbe3d2454c79c8f2356e
chanyaaa58 <kanegonaja@gmail.com>
2021-09-25 17:51:40 +0900

    Userモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_090432) do
+ActiveRecord::Schema.define(version: 2021_09_25_084741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2021_09_21_090432) do
     t.integer "status"
     t.integer "priority"
     t.index ["list"], name: "index_tasks_on_list"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
